68f4e410040e55519c79ad53b41d3a6cdd245641
Abraham <abrahamemmanuelf@gmail.com>
2022-06-24 15:17:56 +0100

    This is my Java Calculator

---
+++ Calculator.js
@@ -0,0 +1,13 @@
+let x,y;
+x = prompt('Enter your first number');
+y = prompt('Enter your second number');
+
+let a = x+y;
+let b = x-y;
+let c = x*y;
+let d = x/y;
+
+alert('The sum of given numbers is:'+a);
+alert('The difference of given numbers is:'+a);
+alert('The product of given numbers is:'+a);
+alert('The quotient of given numbers is:'+a);
